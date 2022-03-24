9b998a47c2e90d2a22000af355e50d15d189bf1d
Akshaya192 <96040710+Akshaya192@users.noreply.github.com>
2022-03-24 09:30:24 +0000

    FIND AREA BY PASS BY REF

---
+++ p6final.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+void input_string(char *a)
+{
+  printf("enter the string\n");
+  scanf("%s",a);
+}
+char str_reverse(char *a)
+{
+  char temp;
+  int i=0,j=0;
+  j=strlen(a)-1; 
+  while(i<j)
+    {
+      temp=a[i];
+      a[i]=a[j];
+      a[j]=temp;
+      i++;
+      j--;
+    }
+}
+void output(char *a)
+{
+  printf("the reverse string is %s", a);
+}
+int main()
+{
+  char a[20];
+  input_string(a);
+  str_reverse(a);
+  output(a);
+  return 0;
+}
