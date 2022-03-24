bf26c958670795551f5db6e35d0561e64c60962c
Akshaya192 <96040710+Akshaya192@users.noreply.github.com>
2022-03-24 09:02:08 +0000

    area of triangle

---
+++ p7final.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+struct _triangle
+{
+float base,altitude,area;
+};
+typedef struct _triangle Triangle;
+Triangle input_triangle()
+{
+  Triangle t;
+  printf("enter base and altitude of triangle\n");
+  scanf("%f%f", &t.base,&t.altitude);
+  return t;
+}
+void find_area(Triangle *t)
+{
+  t->area=0.5*t->base*t->altitude;
+}
+void output(Triangle t)
+{
+  printf("area of triangle with base =%f &altitude=%f is%f\n",t.base,t.altitude,t.area);
+}
+int main()
+{
+  Triangle t=input_triangle();
+  find_area(&t);
+  output(t);
+  return 0;
+}
---
+++ p8final.c
@@ -0,0 +1,72 @@
+[01:41, 24/03/2022] Gayathri Reva: #include<stdio.h>
+struct _triangle
+{
+float base,altitude,area;
+};
+typedef struct _triangle Triangle;
+int input_n()
+{
+  int n;
+  printf("how many triangles will you enter\n");
+  scanf("%d",&n);
+  return n;
+}
+Triangle input_triangle()
+{
+  Triangle t;
+  printf("enter the base and altitude of triangle\n");
+  scanf("%f%f",&t.base,&t.altitude);
+  return t;
+}
+void input_n_triangles(int n,Triangle t[n])
+{
+  for(int i=0;i<n;i++)
+  
+    t[i]=input_triangle();
+  
+}
+void find_area(Triangle *t)
+{
+  t->area=0.5*t->base*t->altitude;
+}
+void find_areas_n(int n,Triangle t[n])
+{
+  for(int i=0;i<n;i++)
+    
+      find_area(&t[i]);
+    
+  }
+Triangle find_smallest_triangle(int n,Triangle t[n])
+{
+  Triangle small;
+  small=t[0];
+  {
+  for(int i=1;i<n;i++)
+    {
+      if(small.area>t[i].area)
+      {
+        small=t[i];
+      }
+    }
+    return small;
+    }
+  }
+  void output(int n,Triangle t[n],Triangle smallest)
+  {
+    for(int i=0;i<n-1;i++)
+      {
+        printf("%f,%f and \n",t[i].base,t[i].altitude);
+      }
+    printf("%f,%f\n is \n triangle with base=%f and altitude=%f having area=%f\n",t[n-1].base,t[n-1].altitude,smallest.base,smallest.altitude,smallest.area);
+    }
+    int main()
+    {
+      int n=input_n();
+      Triangle tris[n];
+      input_n_triangles(n,tris);
+      find_areas_n(n,tris);
+      Triangle small;
+      small=find_smallest_triangle(n,tris);
+      output(n,tris,small);
+      return 0;
+      }
